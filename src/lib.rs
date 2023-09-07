6358d19c6f3bfecd9d729dd66b49dfe41cae6629
chesedo <pieter@chesedo.me>
2023-09-07 08:55:36 +0200

    feat: dynamically render page

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,53 @@
-use axum::{extract::State, http::StatusCode, routing::get, Json, Router};
-use dal::{Blog, Dal};
+use axum::{extract::State, http::StatusCode, response::Html, routing::get, Router};
+use dal::Dal;
+use handlebars::Handlebars;
 use shuttle_runtime::CustomError;
 use sqlx::{migrate::Migrator, PgPool};
+use std::path::PathBuf;
+use tower_http::services::ServeDir;
 use tracing::error;
 
 mod dal;
 
-async fn blog(State(dal): State<Dal>) -> Result<Json<Blog>, StatusCode> {
+async fn blog(
+    State(AppState { dal, templates }): State<AppState>,
+) -> Result<Html<String>, StatusCode> {
     let blog = dal.get_blog("tmp").await.map_err(|error| {
         error!(%error, "failed to get blog from storage");
         StatusCode::NOT_FOUND
     })?;
 
-    Ok(Json(blog))
+    let page = templates.render("blog", &blog).map_err(|error| {
+        error!(%error, "failed to render blog");
+        StatusCode::INTERNAL_SERVER_ERROR
+    })?;
+
+    Ok(Html(page))
 }
 
 static MIGRATOR: Migrator = sqlx::migrate!();
 
-pub async fn app(pool: PgPool) -> Result<Router, CustomError> {
+#[derive(Clone)]
+struct AppState {
+    dal: Dal,
+    templates: Handlebars<'static>,
+}
+
+pub async fn app(pool: PgPool, assets_folder: PathBuf) -> Result<Router, CustomError> {
     MIGRATOR.run(&pool).await.map_err(CustomError::new)?;
+
+    let mut handlebars = Handlebars::new();
+    handlebars.register_templates_directory(".hbs", assets_folder.join("templates"))?;
+
+    let state = AppState {
+        dal: Dal::new(pool),
+        templates: handlebars,
+    };
 
     let router = Router::new()
         .route("/", get(blog))
-        .with_state(Dal::new(pool));
+        .nest_service("/static", ServeDir::new(assets_folder.join("static")))
+        .with_state(state);
 
     Ok(router)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use blog::app;
 use sqlx::PgPool;
+use std::path::PathBuf;
 
 #[shuttle_runtime::main]
-async fn axum(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
-    let app = app(pool).await?;
+async fn axum(
+    #[shuttle_shared_db::Postgres] pool: PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "assets")] assets_folder: PathBuf,
+) -> shuttle_axum::ShuttleAxum {
+    let app = app(pool, assets_folder).await?;
 
     Ok(app.into())
 }
