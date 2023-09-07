c17cb30789c326dc92c56f5f029ca39bd8c8acde
chesedo <pieter@chesedo.me>
2023-09-07 07:16:08 +0200

    misc: initial code

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  # Pin to stable from https://status.nixos.org/
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "moz_overlay_shell";
+    nativeBuildInputs = with nixpkgs; [
+      openssl
+      pkg-config
+    ];
+    buildInputs = with nixpkgs; [
+      (rustChannelOf{ channel = "1.69.0"; }).rust
+      docker
+    ];
+  }
